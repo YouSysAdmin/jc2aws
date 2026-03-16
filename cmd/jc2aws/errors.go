074db1361f18b41d65bcc8b8647c37b5a263f122
YouSysAdmin <git@yousysadmin.com>
2026-03-16 23:27:35 +0200

    remove unused errors

--- cmd/jc2aws/errors.go
+++ cmd/jc2aws/errors.go
@@ -6,7 +6,6 @@ const (
 	AwsRoleArnIsRequired        = "--role-arn is required"
 	AwsPrincipalUrlIsRequired   = "--principal-arnn is required"
 	AwsRegionIsRequired         = "--region is required"
-	MfaRequired                 = "--mfa is required"
 	IdpURLRequred               = "--idp-url is required"
 	AccountNameCantBeUsed       = "--account-name flag can't be used without any pre-configured account"
 	AwsCliProfileNameIsRequired = "--aws-cli-profile-name is required"
